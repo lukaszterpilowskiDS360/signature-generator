e00fcdf2f53a896b9885c02c858ad14bf8efce80
lukaszterpilowskiDS360 <108579210+lukaszterpilowskiDS360@users.noreply.github.com>
2022-08-08 21:40:22 +0200

    atom

--- script.js
+++ script.js
@@ -18,14 +18,14 @@ phoneInput.addEventListener('input', (e) => {
 })
 
 emailInput.addEventListener('input', (e) => {
-    emailField.innerText = e.target.value + '@ds360.pl'
+    emailField.innerText = ' ' + e.target.value + '@ds360.pl'
 })
 
 function formatPhoneNumber(phoneNumberString) {
     var cleaned = ('' + phoneNumberString).replace(/\D/g, '')
     var match = cleaned.match(/^(\d{3})(\d{3})(\d{3})$/)
     if (match) {
-      return  match[1] + ' ' + match[2] + ' ' + match[3]
+      return  match[1] + ' ' + match[2] + ' ' + match[3] + ' '
     }
     return '(proszę wprowadzić cały numer)'
   }
